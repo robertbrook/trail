803b912df32257291dfb8153aadf031c7e9c4979
Robert Brook <robertbrook@fastmail.fm>
2022-03-04 19:10:36 +0000

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9c8e35f1d937d75a3a9f1e0221c23973190d8f39',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9c8e35f1d937d75a3a9f1e0221c23973190d8f39',
+            'dev_requirement' => false,
+        ),
+    ),
+);
