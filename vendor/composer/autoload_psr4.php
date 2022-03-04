b8ccac5a86a1763c22980a7cc7797852f074cdde
Robert Brook <robertbrook@fastmail.fm>
2022-03-04 19:38:48 +0000

    composer require mcmic/gemini-server

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
+    'MCMic\\Gemini\\' => array($vendorDir . '/mcmic/gemini-server/src'),
 );
